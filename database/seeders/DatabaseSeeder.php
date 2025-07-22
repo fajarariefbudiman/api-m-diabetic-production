9461284c42affcf05bfba1226d9c54ea91c4d165
dimsard <d.ardiansyah999@gmail.com>
2025-07-22 16:30:06 +0700

    add education-content

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,8 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             FoodSeeder::class,
-            FactMythSeeder::class
+            FactMythSeeder::class,
+            EducationContentSeeder::class
         ]);
     }
 }
